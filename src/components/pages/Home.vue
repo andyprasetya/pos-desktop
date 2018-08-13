a9daefd24c2bd7305c2933734178b008bf998759
ananech <aribowo.susetyo@gmail.com>
2018-08-13 07:03:52 +0700

    + Add handleGetData and handleCreateChart at Home components

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -4,7 +4,7 @@
       <v-flex xs12 sm6 md3 v-for="stat in stats" :key="stat.title">
         <v-card>
           <v-card-text class="stats">
-            <strong>{{stat.title}}</strong>
+            <strong class="card-title">{{stat.title}}</strong>
             <div class="number">
               {{stat.sum}}
             </div>
@@ -17,11 +17,9 @@
     <v-layout row wrap>
       <v-flex xs12>
         <v-card>
-          <v-card-title primary-title>
-            <strong>Grafik Transaksi Bulan Agustus 2018</strong>
-          </v-card-title>
           <v-card-text>
-            <div id="grafik-transaksi" class="chart big"></div>
+            <strong class="card-title">Grafik Transaksi Bulan {{monthName}} {{year}}</strong>
+            <div id="grafik-transaksi" class="chart"></div>
           </v-card-text>
         </v-card>
       </v-flex>
@@ -37,7 +35,18 @@ import 'amcharts3/amcharts/themes/light';
 export default {
   data () {
     return {
-      stats: [
+      monthName: '',
+      year: 2018,
+      stats: [],
+      chartData: []
+    };
+  },
+  methods: {
+    handleGetData () {
+      // Dummy data
+      this.monthName = 'Agustus';
+      this.year = 2018;
+      this.stats = [
         {
           title: "Penjualan",
           sum: 12345678,
@@ -58,16 +67,192 @@ export default {
           sum: 98393829,
           count: 39
         }
-      ],
-      chartData: [
-        {
-          date: "2018-08-01",
-          jual: ""
-        }
-      ]
-    };
-  },
-  methods: {
+      ];
+
+      this.chartData = [{
+        "date": "2013-01-01",
+        "biaya": 71,
+        "kas": 75,
+        "penjualan": 50,
+        "pembelian": 80
+      }, {
+        "date": "2013-01-02",
+        "biaya": 74,
+        "kas": 78,
+        "penjualan": 40,
+        "pembelian": 60
+      }, {
+        "date": "2013-01-03",
+        "biaya": 78,
+        "kas": 88,
+        "penjualan": 50,
+        "pembelian": 20
+      }, {
+        "date": "2013-01-04",
+        "biaya": 85,
+        "kas": 89,
+        "penjualan": 80,
+        "pembelian": 90
+      }, {
+        "date": "2013-01-05",
+        "biaya": 82,
+        "kas": 89,
+        "penjualan": 90,
+        "pembelian": 60
+      }, {
+        "date": "2013-01-06",
+        "biaya": 83,
+        "kas": 85,
+        "penjualan": 30,
+        "pembelian": 50
+      }, {
+        "date": "2013-01-07",
+        "biaya": 88,
+        "kas": 92,
+        "penjualan": 50,
+        "pembelian": 70
+      }, {
+        "date": "2013-01-08",
+        "biaya": 85,
+        "kas": 90,
+        "penjualan": 70,
+        "pembelian": 60
+      }, {
+        "date": "2013-01-09",
+        "biaya": 85,
+        "kas": 91,
+        "penjualan": 90,
+        "pembelian": 50
+      }, {
+        "date": "2013-01-10",
+        "biaya": 80,
+        "kas": 84,
+        "penjualan": 50,
+        "pembelian": 80
+      }, {
+        "date": "2013-01-11",
+        "biaya": 87,
+        "kas": 92,
+        "penjualan": 40,
+        "pembelian": 80
+      }, {
+        "date": "2013-01-12",
+        "biaya": 84,
+        "kas": 87,
+        "penjualan": 30,
+        "pembelian": 40
+      }, {
+        "date": "2013-01-13",
+        "biaya": 83,
+        "kas": 88,
+        "penjualan": 50,
+        "pembelian": 70
+      }, {
+        "date": "2013-01-14",
+        "biaya": 84,
+        "kas": 87,
+        "penjualan": 50,
+        "pembelian": 80
+      }, {
+        "date": "2013-01-15",
+        "biaya": 81,
+        "kas": 85,
+        "penjualan": 40,
+        "pembelian": 70
+      }, {
+        "date": "2013-01-16",
+        "biaya": 71,
+        "kas": 75,
+        "penjualan": 50,
+        "pembelian": 80
+      }, {
+        "date": "2013-01-17",
+        "biaya": 74,
+        "kas": 78,
+        "penjualan": 40,
+        "pembelian": 60
+      }, {
+        "date": "2013-01-18",
+        "biaya": 78,
+        "kas": 88,
+        "penjualan": 50,
+        "pembelian": 20
+      }, {
+        "date": "2013-01-19",
+        "biaya": 85,
+        "kas": 89,
+        "penjualan": 80,
+        "pembelian": 90
+      }, {
+        "date": "2013-01-20",
+        "biaya": 82,
+        "kas": 89,
+        "penjualan": 90,
+        "pembelian": 60
+      }, {
+        "date": "2013-01-21",
+        "biaya": 83,
+        "kas": 85,
+        "penjualan": 30,
+        "pembelian": 50
+      }, {
+        "date": "2013-01-22",
+        "biaya": 88,
+        "kas": 92,
+        "penjualan": 50,
+        "pembelian": 70
+      }, {
+        "date": "2013-01-23",
+        "biaya": 85,
+        "kas": 90,
+        "penjualan": 70,
+        "pembelian": 60
+      }, {
+        "date": "2013-01-24",
+        "biaya": 85,
+        "kas": 91,
+        "penjualan": 90,
+        "pembelian": 50
+      }, {
+        "date": "2013-01-25",
+        "biaya": 80,
+        "kas": 84,
+        "penjualan": 50,
+        "pembelian": 80
+      }, {
+        "date": "2013-01-26",
+        "biaya": 87,
+        "kas": 92,
+        "penjualan": 40,
+        "pembelian": 80
+      }, {
+        "date": "2013-01-27",
+        "biaya": 84,
+        "kas": 87,
+        "penjualan": 30,
+        "pembelian": 40
+      }, {
+        "date": "2013-01-28",
+        "biaya": 83,
+        "kas": 88,
+        "penjualan": 50,
+        "pembelian": 70
+      }, {
+        "date": "2013-01-29",
+        "biaya": 84,
+        "kas": 87,
+        "penjualan": 50,
+        "pembelian": 80
+      }, {
+        "date": "2013-01-30",
+        "biaya": 81,
+        "kas": 85,
+        "penjualan": 40,
+        "pembelian": 70
+      }];
+      this.handleCreateChart();
+    },
+
     handleCreateChart () {
       window.AmCharts.makeChart("grafik-transaksi", {
         "pathToImages": "https://www.amcharts.com/lib/3/images/",
@@ -77,45 +262,48 @@ export default {
         "precision": 2,
         "valueAxes": [{
           "id": "v1",
-          "title": "Sales",
+          "title": "Nilai",
           "position": "left",
           "autoGridCount": false,
           "labelFunction": function(value) {
-            return "$" + Math.round(value) + "M";
+            return "Rp." + Math.round(value);
           }
         }, {
           "id": "v2",
-          "title": "Market Days",
+          "title": "Nilai",
           "gridAlpha": 0,
           "position": "right",
-          "autoGridCount": false
+          "autoGridCount": false,
+          "labelFunction": function(value) {
+            return "Rp." + Math.round(value);
+          }
         }],
         "graphs": [{
           "id": "g3",
           "valueAxis": "v1",
-          "lineColor": "#e1ede9",
-          "fillColors": "#e1ede9",
+          "lineColor": "#DCE775",
+          "fillColors": "#DCE775",
           "fillAlphas": 1,
           "type": "column",
-          "title": "Actual Sales",
-          "valueField": "sales2",
+          "title": "Pembelian",
+          "valueField": "pembelian",
           "clustered": false,
           "columnWidth": 0.5,
-          "legendValueText": "$[[value]]M",
-          "balloonText": "[[title]]<br /><b style='font-size: 130%'>$[[value]]M</b>"
+          "legendValueText": "Rp.[[value]]",
+          "balloonText": "[[title]]<br /><b style='font-size: 130%'>Rp.[[value]]</b>"
         }, {
           "id": "g4",
           "valueAxis": "v1",
-          "lineColor": "#62cf73",
-          "fillColors": "#62cf73",
+          "lineColor": "#4CAF50",
+          "fillColors": "#4CAF50",
           "fillAlphas": 1,
           "type": "column",
-          "title": "Target Sales",
-          "valueField": "sales1",
+          "title": "Penjualan",
+          "valueField": "penjualan",
           "clustered": false,
           "columnWidth": 0.3,
-          "legendValueText": "$[[value]]M",
-          "balloonText": "[[title]]<br /><b style='font-size: 130%'>$[[value]]M</b>"
+          "legendValueText": "Rp.[[value]]",
+          "balloonText": "[[title]]<br /><b style='font-size: 130%'>Rp.[[value]]</b>"
         }, {
           "id": "g1",
           "valueAxis": "v2",
@@ -127,10 +315,11 @@ export default {
           "lineThickness": 2,
           "lineColor": "#20acd4",
           "type": "smoothedLine",
-          "title": "Market Days",
+          "title": "Biaya",
           "useLineColorForBulletBorder": true,
-          "valueField": "market1",
-          "balloonText": "[[title]]<br /><b style='font-size: 130%'>[[value]]</b>"
+          "valueField": "biaya",
+          "legendValueText": "Rp.[[value]]",
+          "balloonText": "[[title]]<br /><b style='font-size: 130%'>Rp.[[value]]</b>"
         }, {
           "id": "g2",
           "valueAxis": "v2",
@@ -140,13 +329,14 @@ export default {
           "bulletSize": 5,
           "hideBulletsCount": 50,
           "lineThickness": 2,
-          "lineColor": "#e1ede9",
+          "lineColor": "#FF6F00",
           "type": "smoothedLine",
           "dashLength": 5,
-          "title": "Market Days ALL",
+          "title": "Kas",
           "useLineColorForBulletBorder": true,
-          "valueField": "market2",
-          "balloonText": "[[title]]<br /><b style='font-size: 130%'>[[value]]</b>"
+          "valueField": "kas",
+          "legendValueText": "Rp.[[value]]",
+          "balloonText": "[[title]]<br /><b style='font-size: 130%'>Rp.[[value]]</b>"
         }],
         "chartScrollbar": {
           "graph": "g1",
@@ -187,198 +377,18 @@ export default {
         "export": {
          "enabled": true
         },
-        "dataProvider": [{
-          "date": "2013-01-01",
-          "market1": 71,
-          "market2": 75,
-          "sales1": 5,
-          "sales2": 8
-        }, {
-          "date": "2013-01-02",
-          "market1": 74,
-          "market2": 78,
-          "sales1": 4,
-          "sales2": 6
-        }, {
-          "date": "2013-01-03",
-          "market1": 78,
-          "market2": 88,
-          "sales1": 5,
-          "sales2": 2
-        }, {
-          "date": "2013-01-04",
-          "market1": 85,
-          "market2": 89,
-          "sales1": 8,
-          "sales2": 9
-        }, {
-          "date": "2013-01-05",
-          "market1": 82,
-          "market2": 89,
-          "sales1": 9,
-          "sales2": 6
-        }, {
-          "date": "2013-01-06",
-          "market1": 83,
-          "market2": 85,
-          "sales1": 3,
-          "sales2": 5
-        }, {
-          "date": "2013-01-07",
-          "market1": 88,
-          "market2": 92,
-          "sales1": 5,
-          "sales2": 7
-        }, {
-          "date": "2013-01-08",
-          "market1": 85,
-          "market2": 90,
-          "sales1": 7,
-          "sales2": 6
-        }, {
-          "date": "2013-01-09",
-          "market1": 85,
-          "market2": 91,
-          "sales1": 9,
-          "sales2": 5
-        }, {
-          "date": "2013-01-10",
-          "market1": 80,
-          "market2": 84,
-          "sales1": 5,
-          "sales2": 8
-        }, {
-          "date": "2013-01-11",
-          "market1": 87,
-          "market2": 92,
-          "sales1": 4,
-          "sales2": 8
-        }, {
-          "date": "2013-01-12",
-          "market1": 84,
-          "market2": 87,
-          "sales1": 3,
-          "sales2": 4
-        }, {
-          "date": "2013-01-13",
-          "market1": 83,
-          "market2": 88,
-          "sales1": 5,
-          "sales2": 7
-        }, {
-          "date": "2013-01-14",
-          "market1": 84,
-          "market2": 87,
-          "sales1": 5,
-          "sales2": 8
-        }, {
-          "date": "2013-01-15",
-          "market1": 81,
-          "market2": 85,
-          "sales1": 4,
-          "sales2": 7
-        }, {
-          "date": "2013-01-16",
-          "market1": 71,
-          "market2": 75,
-          "sales1": 5,
-          "sales2": 8
-        }, {
-          "date": "2013-01-17",
-          "market1": 74,
-          "market2": 78,
-          "sales1": 4,
-          "sales2": 6
-        }, {
-          "date": "2013-01-18",
-          "market1": 78,
-          "market2": 88,
-          "sales1": 5,
-          "sales2": 2
-        }, {
-          "date": "2013-01-19",
-          "market1": 85,
-          "market2": 89,
-          "sales1": 8,
-          "sales2": 9
-        }, {
-          "date": "2013-01-20",
-          "market1": 82,
-          "market2": 89,
-          "sales1": 9,
-          "sales2": 6
-        }, {
-          "date": "2013-01-21",
-          "market1": 83,
-          "market2": 85,
-          "sales1": 3,
-          "sales2": 5
-        }, {
-          "date": "2013-01-22",
-          "market1": 88,
-          "market2": 92,
-          "sales1": 5,
-          "sales2": 7
-        }, {
-          "date": "2013-01-23",
-          "market1": 85,
-          "market2": 90,
-          "sales1": 7,
-          "sales2": 6
-        }, {
-          "date": "2013-01-24",
-          "market1": 85,
-          "market2": 91,
-          "sales1": 9,
-          "sales2": 5
-        }, {
-          "date": "2013-01-25",
-          "market1": 80,
-          "market2": 84,
-          "sales1": 5,
-          "sales2": 8
-        }, {
-          "date": "2013-01-26",
-          "market1": 87,
-          "market2": 92,
-          "sales1": 4,
-          "sales2": 8
-        }, {
-          "date": "2013-01-27",
-          "market1": 84,
-          "market2": 87,
-          "sales1": 3,
-          "sales2": 4
-        }, {
-          "date": "2013-01-28",
-          "market1": 83,
-          "market2": 88,
-          "sales1": 5,
-          "sales2": 7
-        }, {
-          "date": "2013-01-29",
-          "market1": 84,
-          "market2": 87,
-          "sales1": 5,
-          "sales2": 8
-        }, {
-          "date": "2013-01-30",
-          "market1": 81,
-          "market2": 85,
-          "sales1": 4,
-          "sales2": 7
-        }]
+        "dataProvider": this.chartData
       });
     }
   },
   mounted () {
-    this.handleCreateChart();
+    this.handleGetData();
   }
 }
 </script>
 
 <style lang="css">
-.stats > strong {
+.v-card .card-title {
   display: block;
   font-size: 16px;
   margin: 0 0 4px;
